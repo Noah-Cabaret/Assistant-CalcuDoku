6ac85a4b95f1d39439bbb8dbc6554c764c615cc6
Cabaret <noahcabaret0902@gmail.com>
2026-03-30 14:30:09 +0200

    correction bug

--- calcudoku/src/main/java/fr/univ/calcudoku/model/Case.java
+++ calcudoku/src/main/java/fr/univ/calcudoku/model/Case.java
@@ -11,7 +11,6 @@ import fr.univ.calcudoku.service.aide.visitor.VisiteurGrille;
  * Représente une case de la grille de Calcudoku.
  * Gère sa position, sa valeur actuelle, la solution et les annotations de l'utilisateur.
  */
-
 public class Case implements ElementVisitable {
 
     private final int x;
@@ -19,6 +18,7 @@ public class Case implements ElementVisitable {
     private final IntegerProperty valeur;
     private final int solution;
     private GroupementCases groupement;
+    private boolean estHypothese = false;
 
     private final ObservableSet<Integer> annotations = FXCollections.observableSet(new HashSet<>());
 
@@ -46,6 +46,7 @@ public class Case implements ElementVisitable {
         
         this.valeur = new SimpleIntegerProperty(source.getValeur());
         this.annotations.addAll(source.getNotes());
+        this.estHypothese = source.isEstHypothese();
     }
     
     /**
@@ -61,6 +62,7 @@ public class Case implements ElementVisitable {
      * @return la coordonnée x
      */
     public int getX(){ return x;}
+
     /**
      * Retourne la coordonnée y de la case.
      * @return la coordonnée y
@@ -78,11 +80,13 @@ public class Case implements ElementVisitable {
      * @return la valeur de la case
      */
     public int getValeur(){ return valeur.get();}
+
     /**
      * Définit la valeur de la case.
      * @param valeur la nouvelle valeur
      */
     public void setValeur(int valeur){this.valeur.set(valeur);}
+
     /**
      * Retourne la propriété observable de la valeur.
      * @return la propriété IntegerProperty
@@ -94,11 +98,24 @@ public class Case implements ElementVisitable {
      * @param g le groupement
      */
     public void setGroupement(GroupementCases g) { this.groupement = g; }
+
     /**
      * Retourne le groupement (cage) auquel appartient cette case.
      * @return le groupement
      */
     public GroupementCases getGroupement() { return groupement; }
+
+    /**
+     * Indique si la case est une hypothèse.
+     * @return true si la case est en mode hypothèse
+     */
+    public boolean isEstHypothese() { return estHypothese; }
+
+    /**
+     * Définit si la case est une hypothèse.
+     * @param estHypothese true pour activer le mode hypothèse sur cette case
+     */
+    public void setEstHypothese(boolean estHypothese) { this.estHypothese = estHypothese; }
 
     /**
      * Bascule la visibilité d'une annotation (note) pour la case.
