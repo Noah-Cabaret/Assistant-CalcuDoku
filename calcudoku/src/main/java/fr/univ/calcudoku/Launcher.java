214f2ece06b8e8285be21b4a77270511b80d48d0
Chaosok <kkongchaosok@gmail.com>
2026-04-03 15:51:01 +0200

    pour le .jar

---
+++ calcudoku/src/main/java/fr/univ/calcudoku/Launcher.java
@@ -0,0 +1,8 @@
+package fr.univ.calcudoku;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Appelle la méthode main de votre vraie application
+        MainApp.main(args);
+    }
+}
